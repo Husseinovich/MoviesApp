e6dee833f590980061ebc483cbeb50bf7271689e
Husseinovich <Userdocpopa-1988>
2019-06-16 14:30:29 -0400

    Initial app commit

---
+++ app/src/main/java/com/habdinoor/moviesapp/model/Results.kt
@@ -0,0 +1,15 @@
+package com.habdinoor.moviesapp.model
+import com.squareup.moshi.Json
+
+
+data class Results(
+    @Json(name = "page")
+    val page: Int,
+    @Json(name = "results")
+    val results: List<Result>,
+    @Json(name = "total_pages")
+    val totalPages: Int,
+    @Json(name = "total_results")
+    val totalResults: Int
+)
+
